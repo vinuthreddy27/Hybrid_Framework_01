a2eb5b566bc06b80b57fbe4f1b3dfe19eb517cd7
vinuthreddy <reddyvinuth27@gmail.com>
2024-12-19 23:58:06 -0800

    commit 19

--- yourstore/test_suite/test_login.py
+++ yourstore/test_suite/test_login.py
@@ -2,11 +2,14 @@ from yourstore.POM.home_page import Homepage
 from yourstore.configurations.config import TestData
 
 
+
+
 def test_login(get_browser):
     homepage=Homepage(get_browser)
     login_page=homepage.login_()
     landing_page=login_page.login_page.login_into_application(TestData.email,TestData.password)
     landing_page.proper_msg()
+
 
 
 def test_login2(get_browser):
